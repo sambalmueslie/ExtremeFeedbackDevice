99172deee2c19c27a19654a2f69296afa259483a
Oliver Eckle <me@sambalmueslie.de>
2015-03-26 23:47:58 +0100

    add some comments

--- extreme_feedback_device/src/main/java/de/sambalmueslie/extreme_feedback_device/ExtremeFeedbackController.java
+++ extreme_feedback_device/src/main/java/de/sambalmueslie/extreme_feedback_device/ExtremeFeedbackController.java
@@ -20,9 +20,14 @@ public class ExtremeFeedbackController {
 	 *            the args
 	 */
 	public static void main(final String[] args) {
+		// TODO get this from properties or by args
+		// the host name of the jenkins server
 		final String host = "192.168.2.10";
+		// the jenkins username
 		final String username = "test";
+		// the jenkins password
 		final String password = "test";
+		// the html valid name of the job
 		final String jobName = "Extreme%20Feedback%20Device";
 		new ExtremeFeedbackController(host, username, password, jobName);
 	}
@@ -34,6 +39,7 @@ public class ExtremeFeedbackController {
 		final JenkinsServer jenkins = new JenkinsServer(host, username, password);
 		final CIPollJob pollJob = new CIPollJob(jenkins, jobName, 5000, new LogExtremeFeedbackDevice());
 		pollJob.start();
+		// TODO we need sth to stop this thing
 	}
 
 }
